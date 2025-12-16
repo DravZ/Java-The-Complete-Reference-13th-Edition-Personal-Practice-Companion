d216a60eb7b32a60e3d331239771f8673920f55f
DravZ <cdgo345@gmail.com>
2025-12-16 08:17:52 -0600

    Day 2

---
+++ Example2.java
@@ -0,0 +1,12 @@
+public class Example2 {
+    public static void main(String[] args){
+        int num; //variable called num
+        num = 100; //assigns num the value 100
+
+        System.out.println("This is num: " + num);
+        num = num * 2;
+
+        System.out.print("The calue of num * 2 is ");
+        System.out.println(num);
+    }
+}
