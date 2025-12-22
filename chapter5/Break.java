6b4a198c054f329270dbd2c550eeda60fc2e129c
DravZ <cdgo345@gmail.com>
2025-12-21 22:40:56 -0600

    End of chapter 5

---
+++ chapter5/Break.java
@@ -0,0 +1,19 @@
+package chapter5;
+
+public class Break {
+    public static void main(String[] args) {
+        boolean t = true;
+
+        first: {
+            second: {
+                third: {
+                    System.out.println("Before de break.");
+                    if(t) break second;
+                    System.out.println("This won't execute");
+                }
+                System.out.println("This won't execute");
+            }
+            System.out.println("This is after second block");
+        }
+    }
+}
